6c730f215bc6756c997bfd43ca9f007f16adf232
oier <oieruni22@gmail.com>
2024-03-07 09:11:56 +0100

    [ADD] Making TodoFomrs buttons dynamic, so they can create TODOs and close the form

--- src/TodoForm/index.js
+++ src/TodoForm/index.js
@@ -1,15 +1,39 @@
 import React from "react";
 import './TodoForm.css'
+import { TodoContext } from '../TodoContext'
 
 function TodoForm() {
+  const { addTodo, setOpenModal } = React.useContext(TodoContext)
+  const [newTodoValue, setNewTodoValue] = React.useState('')
+
+  const onSubmit = (event) => {
+    event.preventDefault()
+    addTodo(newTodoValue)
+    setOpenModal(false)
+  }
+
+  const onCancel = (event) => {
+    setOpenModal(false)
+  }
+
+  const onChange = (event) => {
+    setNewTodoValue(event.target.value)
+  }
+
   return (
-    <form>
+    <form onSubmit={onSubmit}>
       <label>Escribe tu nuevo TODO</label>
       <textarea
         placeholder="Hacer curso de React.js"
+        value={newTodoValue}
+        onChange={onChange}
       />
       <div className="TodoForm-buttonContainer">
-        <button className="TodoForm-button TodoForm-button--cancel">Cancelar</button>
+        <button
+          className="TodoForm-button TodoForm-button--cancel"
+          onClick={onCancel}
+        >Cancelar
+        </button>
         <button className="TodoForm-button TodoForm-button--ok">Añadir</button>
       </div>
     </form>
